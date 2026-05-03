9e028f8a1fab647fa8d9a4f84642bc2e26bcbb9f
Jandrei Pflugseder <jandreisp@gmail.com>
2026-05-03 17:08:44 -0300

    ajuste porta render

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.get("/", (req, res) => {
   res.send("Servidor PIX rodando 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
 });
