ebf05b662346c61546c00ffafe839103e0d3f969
Jandrei Pflugseder <jandreisp@gmail.com>
2026-05-03 21:29:04 -0300

    webhook corrigido

--- server.js
+++ server.js
@@ -9,17 +9,33 @@ app.get("/", (req, res) => {
   res.send("Servidor PIX rodando 🚀");
 });
 
-app.post("/webhook", (req, res) => {
+const mercadopago = require("mercadopago");
+
+mercadopago.configure({
+  access_token: "APP_USR-964750908841284-042013-a2357a68ad09b64c752997470d8f29c7-1812220991",
+});
+
+app.post("/webhook", async (req, res) => {
   console.log("Webhook recebido:", JSON.stringify(req.body, null, 2));
 
-  // Aqui você vai tratar pagamento aprovado
-  if (req.body.type === "payment") {
-    console.log("Pagamento recebido!");
+  try {
+    if (req.body.type === "payment") {
+      const paymentId = req.body.data.id;
+
+      const payment = await mercadopago.payment.findById(paymentId);
+
+      console.log("Status do pagamento:", payment.body.status);
+
+      if (payment.body.status === "approved") {
+        console.log("✅ PAGAMENTO APROVADO!");
+      }
+    }
+  } catch (error) {
+    console.error("Erro no webhook:", error.message);
   }
 
   res.sendStatus(200);
 });
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
